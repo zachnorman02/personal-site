d896006aad0c19d4537e322cc8b79369a43a9307
lordfundarbyrd <lordfundarbyrd@gmail.com>
2022-08-30 23:23:03 -0400

    Set up app

---
+++ src/homepage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class HomePage extends React.Component {
+    render() {
+        return (
+            <div id={'homepage-container'}>
+                <h1>Zach Norman</h1>
+                <h3>Software developer and designer</h3>
+                <p>I am a third-year Computer Science and Math major at Northeastern University, expected to graduate
+                    summer 2023. </p>
+                <h2>Software Projects</h2>
+                <h2>Design Projects</h2>
+            </div>
+        )
+    }
+}
+
+export default HomePage;
---
+++ src/navbar.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class NavBar extends React.Component {
+    render() {
+        return (
+            <div id={'navbar'}></div>
+        )
+    }
+}
+
+export default NavBar;
